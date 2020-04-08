ddcc0f96e8101ea9f549e4b667150242bea4ed07
xiao.xinmiao <xiaoxm_001@163.com>
2020-04-08 09:52:29 +0800

    func with param

--- base_folder/func_.py
+++ base_folder/func_.py
@@ -13,12 +13,19 @@ class Book:
 
     def func2(self):
         return 2
+    
+    def func3(self,p):
+        return p
 
 def call(f):
     return getattr(Book(),f)()
+
+def callWithParam(f,p):
+    return getattr(Book(),f)(p)
     
 
 if __name__ == "__main__":
     print(call("func1"))
     print(call("func2"))
+    print(callWithParam("func3",99))
     
